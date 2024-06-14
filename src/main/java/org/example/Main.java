de9ef673e4c78b4f0c3564e999eaaeb94a935320
Ana Lucelly Lizcano <132089765+AnaLizca07@users.noreply.github.com>
2024-06-14 13:18:04 -0500

    Parcial BD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import java.sql.*;
 public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/parcial3";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "";
 
     /**
      * Método principal.
